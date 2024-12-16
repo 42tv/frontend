32fd865b45acfc5a0d4a772bea9d35caae8748d9
eno <k1990121@gmail.com>
2024-12-16 21:32:07 +0900

    a

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -8,18 +8,23 @@ export default function Policy() {
     function fetchPolicy(index: string) {
         switch (index) {
             case 'privacy':
+                setIndex(0);
                 setHtml('개인정보처리방침');
                 break;
             case 'terms':
+                setIndex(1);
                 setHtml('이용약관');
                 break;
             case 'youth':
+                setIndex(2);
                 setHtml('청소년보호정책');
                 break;
             case 'watch':
+                setIndex(3);
                 setHtml('시청기준안내');
                 break;
             case 'broadcast':
+                setIndex(4);
                 setHtml('방송기준안내');
                 break;
             default:
