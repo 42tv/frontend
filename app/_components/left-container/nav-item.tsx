1ba9503da06a05cddf67d77f12a309167f3470d1
eno <k1990121@gmail.com>
2025-02-20 20:04:48 +0900

    배경 이상한거 석인거제거

--- app/_components/left-container/nav-item.tsx
+++ app/_components/left-container/nav-item.tsx
@@ -10,7 +10,7 @@ const NavItem: React.FC<NavItemProps> = ({ icon: Icon, label }) => {
   return (
     <div
       className={`flex w-full pl-8 items-center p-2 space-x-4 cursor-pointer 
-      bg-white dark:bg-[#121212] dark:bg-gray-900 
+      bg-white dark:bg-[#121212] 
       hover:bg-gray-400 dark:hover:bg-gray-800`}
     >
       <Icon className="w-5 h-5" />
