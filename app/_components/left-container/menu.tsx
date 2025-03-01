0331bc914668c950487bdc60b6dd4fd863d53a52
eno <k1990121@gmail.com>
2025-03-02 07:35:19 +0900

    비디오 아이콘 변경

--- app/_components/left-container/menu.tsx
+++ app/_components/left-container/menu.tsx
@@ -1,10 +1,10 @@
 import NavItem from "./nav-item";
-import VideoIcon from "./video-icon";
+import { BsBroadcastPin } from "react-icons/bs";
 
 export default function Menu() {
     return (
         <div className="flex flex-col ">
-            <NavItem icon={VideoIcon} label="전체 방송" />
+            <NavItem icon={BsBroadcastPin} label="전체 방송" />
         </div>
     )
 }
