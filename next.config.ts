d99ac049f6bdf1a3b3a0f79173c0276eaa4e8b75
eno <k1990121@gmail.com>
2025-04-17 20:09:18 +0900

    cdn 주소로 이미지 다운로드 허용

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: {},
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_CDN_URL || "", // 기본값으로 빈 문자열을 제공하거나 에러 처리를 할 수 있습니다.
+        port: "",
+        pathname: "/**", // 필요에 따라 경로를 더 구체적으로 지정할 수 있습니다.
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
