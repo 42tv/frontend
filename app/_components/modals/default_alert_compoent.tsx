be45d939b21351775545b79cf02388b6078e8cec
eno <k1990121@gmail.com>
2025-06-22 02:55:44 +0900

    fix: default alert message 배경색 변경

--- app/_components/modals/default_alert_compoent.tsx
+++ app/_components/modals/default_alert_compoent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function DefaultAlertMessage({ message }: { message: string }) {
   return (
     <div
-      className="flex max-w-md p-5 border rounded-lg border-contentBg bg-contentBg relative text-center items-center justify-center" // Added text-left class for left alignment
+      className="flex max-w-md p-5 border rounded-lg border-contentBg bg-colorFg01 relative text-center items-center justify-center" // Added text-left class for left alignment
     >
         {message}
     </div>
