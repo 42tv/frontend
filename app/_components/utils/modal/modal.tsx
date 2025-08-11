1b7f7855651d11c404ecf7966f98aab3daf8bdb9
eno <k1990121@gmail.com>
2025-08-11 21:22:29 +0900

    overlay-kit demo

--- app/_components/utils/modal/modal.tsx
+++ app/_components/utils/modal/modal.tsx
@@ -1,27 +1,38 @@
 'use client';
 import { MdClose } from "react-icons/md";
 import useModalStore from "../store/modalStore";
+import { OverlayProvider, OverlayControllerComponent } from 'overlay-kit';
 
-const Modal = () => {
-    const { isOpen, closeModal, content } = useModalStore();
+const OverlayModalController: OverlayControllerComponent = ({ isOpen, close }) => {
+    const { content } = useModalStore();
 
     if (!isOpen) return null;
 
     return (
-        <div
-            className="fixed inset-0 flex justify-center items-center z-50"
-        >
+        <div className="fixed inset-0 flex justify-center items-center z-50">
             <div className="rounded-lg shadow-lg relative">
                 {/* X 버튼 */}
                 <button
                     className="absolute w-[25px] h-[25px] top-2 right-2 flex justify-center items-center cursor-pointer z-20"
-                    onClick={closeModal}
+                    onClick={close}
                 >
                     <MdClose className="w-full h-full" />
                 </button>
                 {content}
             </div>
         </div>
+    );
+};
+
+const Modal = () => {
+    const { isOpen, closeModal } = useModalStore();
+
+    if (!isOpen) return null;
+
+    return (
+        <OverlayProvider>
+            <OverlayModalController isOpen={isOpen} close={closeModal} overlayId="modal" unmount={() => {}} />
+        </OverlayProvider>
     );
 };
 
