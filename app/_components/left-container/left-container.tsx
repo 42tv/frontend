1719242002cbb16949ba53742e68705ee59e9121
eno <k1990121@gamil.com>
2025-10-25 02:31:40 +0900

    fix: left-contianer 횡 스크롤에 이동 안하게 변경

--- app/_components/left-container/left-container.tsx
+++ app/_components/left-container/left-container.tsx
@@ -6,8 +6,8 @@ import Menu from './menu';
 
 const LeftContainer: React.FC = () => {
   return (
-    // fixed 제거, w-[200px] h-full 유지, border-r 추가
-    <div className="sticky left-0 w-[200px] h-[calc(100vh-65px)] flex-shrink-0 bg-background dark:bg-background-dark border-r border-border-primary z-10"> {/* 전체 화면에서 toolbar 높이(65px)를 단 높이 */}
+    // fixed 포지션으로 스크롤에 영향받지 않고 고정, min-h로 전체 높이 확보하여 border가 끊기지 않도록 처리
+    <div className="fixed left-0 top-[65px] w-[200px] min-h-[calc(100vh-65px)] flex-shrink-0 bg-background dark:bg-background-dark border-r border-border-primary z-10">
         <Menu />
         {/* <BookMark /> */}
         {/* <Recommend /> */}
